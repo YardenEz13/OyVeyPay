a67e8d9c936c5cc33d515f172239257537765de5
YardenEz13 <yarden.etz@gmail.com>
2025-02-27 12:33:09 +0200

    added print

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -26,6 +26,7 @@ createRoot(document.getElementById('root')).render(
         />
         {console.log("is in prod: ",process.env.NODE_ENV)}
         {console.log("server link: ",baseURL = process.env.NODE_ENV === 'production' ? 'https://oyveypay.onrender.com/api' : 'http://localhost:1312/api')}
+        {console.log("end")}
       </AuthProvider>
     </BrowserRouter>
   
