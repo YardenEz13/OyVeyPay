4dff2b6e5f970e8404d6c9c58c023ae7ada16b01
YardenEz13 <yarden.etz@gmail.com>
2025-02-27 12:21:01 +0200

    added print in prod

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -24,6 +24,7 @@ createRoot(document.getElementById('root')).render(
           pauseOnHover
           theme="dark"
         />
+        {console.log(process.env.NODE_ENV)}
       </AuthProvider>
     </BrowserRouter>
   
