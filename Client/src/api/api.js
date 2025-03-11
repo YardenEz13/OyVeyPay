1c11a994f7cffa53d3c92b1cc6c4327c54b57ede
YardenEz13 <yarden.etz@gmail.com>
2025-03-11 11:29:04 +0200

    Configure Vite build output directory and update API base URL for production

--- Client/src/api/api.js
+++ Client/src/api/api.js
@@ -1,10 +1,13 @@
 import axios from 'axios'
 
+const baseURL =
+process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:1312/api'
 export default axios.create({
-    baseURL:'http://localhost:1312/api',
+    baseURL: baseURL,
     headers:{
         'Content-type':'application/json'
     },
-    withCredentials:true
+    withCredentials:true,
+    
     
 })
